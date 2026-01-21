3964fc71639960cb3962975da118d34a1ada18c8
Martin Perfler <machtl@Machtls-M1-9.local>
2026-01-20 18:32:03 -0800

    test

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "github-pages", group: :jekyll_plugins
 gem "jekyll-include-cache"
 gem "minimal-mistakes-jekyll"
 
+
 # fixes for newer Ruby versions
 gem "bigdecimal"
 gem "csv"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Why I started this blog",
+        "excerpt":"I wanted a place to write about snow, mountains, and research without the constraints of formal papers.  ","categories": [],
+        "tags": [],
+        "url": "/2026/01/19/welcome.html",
+        "teaser": null
+      }]
